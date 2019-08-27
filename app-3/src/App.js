378e3b6e9bb25da9fe441208d0e3902eb22adf93
taylor dearden <taylordearden@gmail.com>
2019-08-27 15:33:24 -0600

    finished app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,10 +3,31 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      theList: '',
+      bestThings: ['Dr Pepper', 'More Dr Pepper', 'Even more Dr Pepper', 'Endless Dr Pepper']
+    }
+  }
+
+  listChange(value) {
+    this.setState({ theList: value })
+  }
+
   render() {
+    let thingsDisplay = this.state.bestThings.filter((element, index) => {
+      return element.includes(this.state.theList)
+    })
+    let thingsDisplay2 = thingsDisplay.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+
     return (
       <div className="App">
-       
+        <input onChange={e => this.listChange(e.target.value)} type='text' />
+        {thingsDisplay2}
       </div>
     );
   }
