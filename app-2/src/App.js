29af5719bd13a903515753cdcf62197730073c14
taylor dearden <taylordearden@gmail.com>
2019-08-27 14:55:17 -0600

    finished 1 and 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state= {
+      cows: ['one cow','two cow', 'three cow','four cow', 'five cow']
+    }
+  }
+  
+  
+  
   render() {
+    let cowParty= this.state.cows.map((element, index)=>{
+      return <h2 key={index}>{element}</h2>
+    })
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App">{cowParty}
       </div>
     );
   }
